e9a1f02bbf1de839481f6002c3021566345b2ece
iqbalthex <iqbalariem232@gmail.com>
2023-12-30 07:10:14 +0700

    init adonis

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,8 @@
+//import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  // public async login(ctx: HttpContextContract) {
+  async login() {
+    return { tes: 'uhuy' }
+  }
+}
